d10242e5bf6a5f92e5f8c68b5bbe51127d20528b
catlyyyyyyyyyyyyyyyyyyyyyy <comnikumi@gmail.com>
2025-11-12 20:53:40 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
@@ -53,6 +53,10 @@ export default {
                         <li>
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
+                        </li>
+                         <li>
+                            <div class="type-title-sm">Picture</div>
+                            <p>{{ level.picture || 'Needed' }}</p>
                         </li>
                     </ul>
                     <h2>Records</h2>
@@ -100,28 +104,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Needs proof the user has beeten the level, or a record
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Use the real level, or an ULDM copy, and show proof of the level
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Audible clicks are optional
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Videos need to be clear, needs to show the death before the winning attempt
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Needs to show level completion, and back out to show the level.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Use the actual path and not a swag route
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        No easy versions of levels
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Legacy list levels will not be accepted
                     </p>
                 </div>
             </div>
